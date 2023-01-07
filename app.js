b5b582e49baf220bf2df7750d89adb4aa44dfb4b
Evgeniy (Bereza laptop) <evgeniyworkbel@gmail.com>
2023-01-07 15:14:47 +0300

    Add dependency fastify-static, connect react-app to backend

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
+const fastifyStatic = require('@fastify/static');
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
-
+  fastify.register(fastifyStatic, {
+    root: `${process.cwd()}/my-app/build`,
+  });
+  fastify.setNotFoundHandler((req, res) => {
+    res.sendFile('index.html');
+  });
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
